30d40555a1c79afb8fff8c159f03152708b9f571
sabir learning <sabir2014852@gmail.com>
2026-05-14 13:12:22 +0500

    this is the code of array

---
+++ chngevalarr.js
@@ -0,0 +1,26 @@
+let sabir=["he is good man","He is very honest man"];
+{
+    sabir[0]="my name is";
+    console.log(sabir)
+    console.log(sabir.indexOf("sabir"));
+}
+console.log("*****We can push a vale in the array")
+sabir.push("sabir");
+console.log(sabir);
+console.log(sabir.indexOf("sabir"));
+console.log("****Now i basic understand of Array and can do some practice****");
+let numbers=["sabir","2","ali","user","hello","given","we can't","we can not","hey","23"];
+console.log("***** I given below i just show the full array which values are done******");
+console.log(sabir);
+console.log("****in this code we tell the full lenght of the array*****");
+console.log(numbers.length)
+console.log("******in this code i find the index number of my array values*****")
+
+console.log("*****In this code we give the value and find the which is value if present in this numbers**** ");
+console.log(numbers[4]);
+console.log("*****in this code we push a number into the array******");
+numbers.push("fida");
+console.log(numbers);
+console.log("*****In this code i will pull the value from the array");
+
+console.log(numbers);
