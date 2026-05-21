7879e09611b85bf66c40b896f7d65fd8905b6b50
sabir learning <sabir2014852@gmail.com>
2026-05-21 15:33:23 +0500

    3.2.1 in the node type text, element, and comment

---
+++ nodedoc.js
@@ -0,0 +1,6 @@
+var newelement = document.createElement("p");
+var newtext= document.createTextNode("This is the code of node conceipt");
+var newcomment = document.createComment("This is the comment of");
+console.log(newtext);
+console.log(newelement);
+console.log(newcomment);
